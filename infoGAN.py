512a2cc3aa547a163d2ee42e4de3db11f3ee03c2
do-hyeon, song <bibiairhd@gmail.com>
2024-12-01 20:48:50 +0900

    InfoGAN 돈다

--- infoGAN.py
+++ infoGAN.py
@@ -6,6 +6,7 @@ import os
 import glob
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
+from makeDataset import createTfDataset
 
 # 하이퍼파라미터
 batch_size = 16
@@ -13,8 +14,8 @@ image_height = 256
 image_width = 128
 channels = 3
 latent_dim = 100
-categorical_dim = 10  # 예: 10개의 범주
-continuous_dim = 2    # 예: 2개의 연속 변수
+categorical_dim = 2  # 예: 10개의 범주
+continuous_dim = 0    # 예: 2개의 연속 변수
 total_dim = latent_dim + categorical_dim + continuous_dim
 buffer_size = 1000
 num_epochs = 50
@@ -23,21 +24,15 @@ sample_interval = 100
 # 데이터 디렉토리 설정 (이미지가 저장된 디렉토리 경로로 변경)
 data_dir = './data/images'  # 예: './data/images' 디렉토리에 이미지가 저장되어 있다고 가정
 
+visualize = False
+save = True
 # 이미지 데이터셋 로드 및 전처리
-train_dataset = image_dataset_from_directory(
-    data_dir,
-    labels=None,  # 레이블이 없는 경우
-    label_mode=None,
-    batch_size=batch_size,
-    image_size=(image_height, image_width),
-    shuffle=True,
-    seed=123
-)
+train_dataset = createTfDataset()
 
 # 데이터 정규화: [-1, 1] 범위로 정규화
-normalization_layer = layers.Rescaling(1./127.5, offset=-1)
-train_dataset = train_dataset.map(lambda x: normalization_layer(x))
-train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+# normalization_layer = layers.Rescaling(1./127.5, offset=-1)
+# train_dataset = train_dataset.map(lambda x: normalization_layer(x))
+# train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 
 def build_generator(input_dim, output_shape=(256, 128, 3)):
@@ -76,7 +71,8 @@ def build_generator(input_dim, output_shape=(256, 128, 3)):
     
     # Upsampling to (256, 128, 3)
     model.add(layers.Conv2DTranspose(3, kernel_size=4, strides=2, padding='same', use_bias=False))
-    model.add(layers.Activation('tanh'))  # 출력 범위: [-1, 1]
+    model.add(layers.Activation('sigmoid'))  # 출력 범위: [0, 1]
+
     assert model.output_shape == (None, 256, 128, 3)
     
     return model
@@ -117,13 +113,19 @@ def build_discriminator_with_q(input_shape=(256, 128, 3), categorical_dim=10, co
     # 범주형 변수에 대한 로짓
     q_logits = layers.Dense(categorical_dim, name='c_logits')(features)
     
-    # 연속 변수에 대한 평균
-    q_mu = layers.Dense(continuous_dim, name='s_mu')(features)
+    # 연속 변수가 없으므로 s_mu와 s_var는 생략
+    # continuous_dim = 0인 경우
+    if continuous_dim > 0:
+        # 연속 변수에 대한 평균
+        q_mu = layers.Dense(continuous_dim, name='s_mu')(features)
+        
+        # 연속 변수에 대한 분산 (양수 유지)
+        q_var = layers.Dense(continuous_dim, activation='softplus', name='s_var')(features)
+        
+        model = models.Model(inputs=input_img, outputs=[D_output, q_logits, q_mu, q_var], name='Discriminator_with_Q')
+    else:
+        model = models.Model(inputs=input_img, outputs=[D_output, q_logits], name='Discriminator_with_Q')
     
-    # 연속 변수에 대한 분산 (양수 유지)
-    q_var = layers.Dense(continuous_dim, activation='softplus', name='s_var')(features)
-    
-    model = models.Model(inputs=input_img, outputs=[D_output, q_logits, q_mu, q_var], name='Discriminator_with_Q')
     return model
 
 
@@ -134,7 +136,7 @@ mse_loss = losses.MeanSquaredError()
 
 # 모델 초기화
 generator = build_generator(latent_dim + categorical_dim + continuous_dim)
-discriminator = build_discriminator_with_q()
+discriminator = build_discriminator_with_q(input_shape=(256, 128, 3) , categorical_dim =categorical_dim, continuous_dim = continuous_dim)
 
 # 최적화기
 lr = 2e-4
@@ -155,22 +157,26 @@ def sample_latent(batch_size):
     c_onehot = np.zeros((batch_size, categorical_dim))
     c_onehot[np.arange(batch_size), c] = 1
     
-    # 연속 변수 s (균일 분포 [-1, 1])
-    s = np.random.uniform(-1, 1, (batch_size, continuous_dim))
+    # 연속 변수 s (연속 변수 없음)
+    if continuous_dim > 0:
+        s = np.random.uniform(-1, 1, (batch_size, continuous_dim))
+        latent = np.concatenate([z, c_onehot, s], axis=1)
+    else:
+        latent = np.concatenate([z, c_onehot], axis=1)
     
-    latent = np.concatenate([z, c_onehot, s], axis=1)
-    return latent, c, s
+    return latent, c
 
 # 훈련 단계 정의
-@tf.function
+# @tf.function
 def train_step(real_images):
-    batch_size_current = tf.shape(real_images)[0]
+    batch_size_current = int(tf.shape(real_images)[0])
+    # batch_size_current = real_images[0].shape[0]
     
     # 잠재 변수 샘플링
-    latent, c, s = sample_latent(batch_size_current)
+    latent, c = sample_latent(batch_size_current)
     latent = tf.convert_to_tensor(latent, dtype=tf.float32)
     c_onehot = tf.one_hot(c, depth=categorical_dim)
-    s = tf.convert_to_tensor(s, dtype=tf.float32)
+    # s = tf.convert_to_tensor(s, dtype=tf.float32)
     
     # 진짜와 가짜 레이블 (원-핫 인코딩)
     real_labels = tf.one_hot([0] * batch_size_current, depth=2)  # 진짜: [1, 0]
@@ -178,14 +184,21 @@ def train_step(real_images):
     
     with tf.GradientTape(persistent=True) as tape:
         # 진짜 이미지에 대한 Discriminator 출력
-        D_real, _, _, _ = discriminator(real_images, training=True)
+        if continuous_dim >0:
+            D_real, _, _, _ = discriminator(real_images, training=True)
+        else:
+            D_real, _, = discriminator(real_images, training=True)
+
         loss_real = cross_entropy(real_labels, D_real)
         
         # 가짜 이미지 생성
         fake_images = generator(latent, training=True)
         
         # 가짜 이미지에 대한 Discriminator 출력
-        D_fake, q_logits_fake, q_mu_fake, q_var_fake = discriminator(fake_images, training=True)
+        if continuous_dim >0:
+            D_fake, q_logits_fake, q_mu_fake, q_var_fake = discriminator(fake_images, training=True)
+        else:
+            D_fake, q_logits_fake = discriminator(fake_images, training=True)
         loss_fake = cross_entropy(fake_labels, D_fake)
         
         # 총 Discriminator 손실
@@ -199,9 +212,10 @@ def train_step(real_images):
         loss_c = categorical_cross_entropy(c_onehot, q_logits_fake)
         
         # 연속 변수에 대한 손실 (MSE)
-        loss_s = mse_loss(s, q_mu_fake)
+        # loss_s = mse_loss(s, q_mu_fake)
         
-        loss_Q = loss_c + loss_s
+        # loss_Q = loss_c + loss_s
+        loss_Q = loss_c
         
         # 총 Generator 손실
         loss_G_total = loss_G + loss_Q  # 하이퍼파라미터 조정 가능
@@ -218,30 +232,46 @@ def train_step(real_images):
 
 # 학습 루프
 for epoch in range(num_epochs):
-    for step, real_images in enumerate(train_dataset):
+    for step, (real_images, _) in enumerate(train_dataset):
         loss_D, loss_G, loss_Q = train_step(real_images)
         
         # 진행 상황 출력
-        if step % 100 == 0:
-            print(f"Epoch [{epoch+1}/{num_epochs}] Step {step} \
-                  Loss D: {loss_D.numpy():.4f}, Loss G: {loss_G.numpy():.4f}, Loss Q: {loss_Q.numpy():.4f}")
+        # if step % 100 == 0:
+        print(f"Epoch [{epoch+1}/{num_epochs}] Step {step} \
+                Loss D: {loss_D.numpy():.4f}, Loss G: {loss_G.numpy():.4f}, Loss Q: {loss_Q.numpy():.4f}")
     
     # 에포크 끝날 때마다 샘플 이미지 저장
     if (epoch + 1) % 1 == 0:
         num_samples = 10
         # 잠재 변수 생성 (c는 서로 다른 클래스, s는 0)
         z = np.random.randn(num_samples, latent_dim)
-        c_fixed = np.eye(categorical_dim)[:num_samples]
-        s_fixed = np.zeros((num_samples, continuous_dim))
-        latent_fixed = np.concatenate([z, c_fixed, s_fixed], axis=1)
+        c_fixed = np.tile(np.eye(categorical_dim), (int(np.ceil(num_samples / categorical_dim)), 1))[:num_samples]
+        if continuous_dim >0:
+            s_fixed = np.zeros((num_samples, continuous_dim))
+            latent_fixed = np.concatenate([z, c_fixed, s_fixed], axis=1)
+        else:
+            latent_fixed = np.concatenate([z, c_fixed], axis=1)
         latent_fixed = tf.convert_to_tensor(latent_fixed, dtype=tf.float32)
         
         generated_images = generator(latent_fixed, training=False)
         generated_images = (generated_images + 1.0) / 2.0  # [0,1] 범위로 변환
         
-        # 이미지 시각화
-        fig, axes = plt.subplots(1, num_samples, figsize=(num_samples * 2, 2))
-        for img, ax in zip(generated_images, axes):
-            ax.imshow(img.numpy())
-            ax.axis('off')
-        plt.show()
+        if visualize == True:
+            # 이미지 시각화
+            fig, axes = plt.subplots(1, num_samples, figsize=(num_samples * 2, 2))
+            
+            for img, ax in zip(generated_images, axes):
+                ax.imshow(img.numpy())
+                ax.axis('off')
+            plt.show()
+        # 이미지 저장
+        if save == True:
+            for i, img in enumerate(generated_images):
+                # img_array = img.numpy()  # 텐서를 numpy 배열로 변환
+                # plt.imshow(img_array)
+                # plt.axis('off')
+
+                # 이미지 저장
+                save_path = os.path.join("generatedImage", f"generated_image_{i+1}.png")
+                plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
+                plt.close()  # 현재 figure를 닫아 메모리 절약
---
+++ makeDataset.py
@@ -0,0 +1,70 @@
+import os
+import tensorflow as tf
+from preprocess import parse_and_process_file
+
+
+# 데이터셋 경로 설정
+base_path = "/Users/songdohyeon/Downloads/dataverse_files"
+measurement_series = [
+    "measurementSeries_B",
+    "measurementSeries_C",
+    "measurementSeries_D",
+    "measurementSeries_E",
+    "measurementSeries_F"
+]
+tightening_levels = ["05cNm", "10cNm", "20cNm", "30cNm", "40cNm", "50cNm", "60cNm"]
+
+# 라벨 매핑
+label_mapping = {
+    "05cNm": 0,
+    "10cNm": 0,
+    "20cNm": 0,
+    "30cNm": 0,
+    "40cNm": 0,
+    "50cNm": 1,
+    "60cNm": 1
+}
+
+# 파일 경로와 라벨 리스트 생성
+file_paths = []
+labels = []
+
+for series in measurement_series:
+    for level in tightening_levels:
+        folder_path = os.path.join(base_path, series, level)
+        if not os.path.exists(folder_path):
+            continue
+        mat_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".mat")]
+        file_paths.extend(mat_files)
+        labels.extend([label_mapping[level]] * len(mat_files))
+
+# TensorFlow 데이터셋으로 변환
+file_paths = tf.constant(file_paths)
+labels = tf.constant(labels, dtype=tf.int32)
+batch_size = 16
+nperseg = 1024
+noverlap = 768
+original_shape = (512,256)
+downsampled_shape = (256, 128)  # Downsampled shape
+buffer_size = 100
+num_classes = 2
+def createTfDataset():
+    # TensorFlow 데이터셋 생성
+    dataset = tf.data.Dataset.from_tensor_slices((file_paths, labels))
+    dataset = dataset = dataset.map(
+            lambda file_path, label: (
+                parse_and_process_file(file_path, nperseg, noverlap, original_shape),
+                tf.one_hot(label, depth=num_classes)  # 레이블을 원-핫 인코딩
+            ),
+            num_parallel_calls=tf.data.AUTOTUNE
+        )
+    dataset = dataset.map(
+            lambda x, y: (tf.image.resize(x, downsampled_shape), y),
+            num_parallel_calls=tf.data.AUTOTUNE
+        )
+
+        # Prefetch to improve performance
+    dataset = dataset.shuffle(buffer_size=buffer_size)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    return dataset
